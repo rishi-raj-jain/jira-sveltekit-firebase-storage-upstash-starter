a08188095dc644c2437aecef678e4bb15ac879e5
Rishi Raj Jain <rjain@llnw.com>
2023-06-07 14:51:01 +0530

    update syntax

--- src/lib/upstash/setup.ts
+++ src/lib/upstash/setup.ts
@@ -1,5 +1,9 @@
 import { Redis } from '@upstash/redis'
+import { UPSTASH_REDIS_REST_URL, UPSTASH_REDIS_REST_TOKEN } from '$env/static/private'
 
-const redis = Redis.fromEnv()
+const redis = new Redis({
+	url: UPSTASH_REDIS_REST_URL,
+	token: UPSTASH_REDIS_REST_TOKEN
+})
 
 export default redis
